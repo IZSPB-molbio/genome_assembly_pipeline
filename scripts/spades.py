8cd98292f92f2eb6fc13d7da149de8ba6d1dd8d8
Domenico Simone <dome.simone@gmail.com>
2021-06-21 20:41:26 +0200

    scripts/spades.py: sys --> snakemake

--- scripts/spades.py
+++ scripts/spades.py
@@ -1,10 +1,15 @@
 import os, sys
 from snakemake import shell
 
-input_R1 = sys.argv[1]
-input_R2 = sys.argv[2]
-input_U  = sys.argv[3]
-outdir   = sys.argv[4]
+# input_R1 = sys.argv[1]
+# input_R2 = sys.argv[2]
+# input_U  = sys.argv[3]
+# outdir   = sys.argv[4]
+
+input_R1 = snakemake.input.R1
+input_R2 = snakemake.input.R2
+input_U  = snakemake.input.U
+outdir   = snakemake.params.outdir
 
 if len(input_R1) > 1:
     pe1 = ""
